b790631564832e76e45f815842956634337b5c0d
Uncle Bamdad <mohammadJavadTabari1024@outlook.com>
2025-05-25 08:16:39 -0700

    edit command

---
+++ DataLayer/Base/Command/EditCommand.cs
@@ -0,0 +1,20 @@
+﻿using DataLayer.Base.Response;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataLayer.Base.Command;
+public class EditCommand : IRequest<ResponseDto<string>>
+{
+	[Display(Name = "آیدی")]
+	public long id { get; set; }
+
+	[Display(Name = "نامک")]
+	public string? slug { get; set; }
+
+	[Display(Name = "نام")]
+	[Required(ErrorMessage = "لطفا مقدار {0} را وارد کنید")]
+	public string name { get; set; }
+
+	[Display(Name = "زمان ویرایش")]
+	public DateTime updated_at { get; set; } = DateTime.Now;
+}
