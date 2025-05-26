27f1ca8672c917e14aeb40e5d9169e70f966a59e
Uncle Bamdad <mohammadJavadTabari1024@outlook.com>
2025-05-26 09:36:34 -0700

    DeleteBlogCommandHandlerTests

---
+++ 304.Net.Platform.Test/TestHandlers/BlogTests/DeleteBlogCommandHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using _304.Net.Platform.Application.BlogFeatures.Command;
+using _304.Net.Platform.Application.BlogFeatures.Handler;
+using _304.Net.Platform.Test.DataProvider;
+using _304.Net.Platform.Test.GenericHandlers;
+using Core.EntityFramework.Models;
+using DataLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _304.Net.Platform.Test.TestHandlers.BlogTests;
+public class DeleteBlogCommandHandlerTests
+{
+	[Fact]
+	public async Task Handle_ShouldDeleteBlog_WhenExists()
+	{
+		var command = BlogDataProvider.Delete();
+
+		await DeleteHandlerTestHelper.TestDelete<
+			DeleteBlogCommand,
+			Blog,
+			IBlogRepository,
+			DeleteBlogCommandHandler>(
+			handlerFactory: uow => new DeleteBlogCommandHandler(uow),
+			execute: (handler, cmd, token) => handler.Handle(cmd, token),
+			command: command,
+			repoSelector: uow => uow.BlogRepository
+		);
+	}
+
+	[Fact]
+	public async Task Handle_ShouldReturnNotFound_WhenBlogDoesNotExist()
+	{
+		var command = BlogDataProvider.Delete(id: 99);
+
+		await DeleteHandlerTestHelper.TestDeleteNotFound<
+			DeleteBlogCommand,
+			Blog,
+			IBlogRepository,
+			DeleteBlogCommandHandler>(
+			handlerFactory: uow => new DeleteBlogCommandHandler(uow),
+			execute: (handler, cmd, token) => handler.Handle(cmd, token),
+			command: command,
+			repoSelector: uow => uow.BlogRepository
+		);
+	}
+}
